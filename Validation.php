e4d6d254401ef661188a0cfc37c4882f339c1d85
nicolascommegna <34098364+nicolascommegna@users.noreply.github.com>
2017-12-19 11:48:35 +0100

    Add files via upload

---
+++ Validation.php
@@ -0,0 +1,16 @@
+<?php
+	require_once("Gump.validation.php");
+
+	class Validation extends Gump_validation
+	{
+		public function mysql_escape()
+		{
+			
+		}
+
+		public function validate_myvalidator()
+		{
+			
+		}
+
+	}
